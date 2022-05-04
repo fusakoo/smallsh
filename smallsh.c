f84717feea9dbe06395aad3fe3e43228d237b3e5
fusakoo <obatafusako@gmail.com>
2022-05-04 00:33:12 -0700

    Need to figuer out the \n situation, but arguments are passing through

--- smallsh.c
+++ smallsh.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define BUFFSIZE 2048
 #define ARGSIZE 512
@@ -52,44 +53,62 @@ int main(){
         }
         fflush(stdout);
       } else {
+        bool background = false;
+
         /* Otherwise, check the arguments */
         int inputarg = 0;
         struct input* currInput = malloc(sizeof(struct input));
-        //currInput->args = malloc(sizeof(char) * inputsize);
         for (int i = 0; i < inputsize; i++) {
           /* Command */
           if (i == 0) {
             currInput->command = calloc(strlen(inputs[i]) + 1, sizeof(char));
+            //char* command;
+            //sscanf(inputs[i], "%s", command);
+            //strcpy(currInput->command, command);
             strcpy(currInput->command, inputs[i]);
           }
           /* Input file */
           else if (strcmp(inputs[i], "<") == 0) {
             i++;
             currInput->input = calloc(strlen(inputs[i]) + 1, sizeof(char));
+            //char* infile; 
+            //sscanf(inputs[i],"%s",infile);
+            //strcpy(currInput->input, infile);
             strcpy(currInput->input, inputs[i]);
           }
           /* Output file */
           else if (strcmp(inputs[i], ">") == 0) {
             i++;
             currInput->output = calloc(strlen(inputs[i]) + 1, sizeof(char));
-            char* outfile;
-            sscanf(inputs[i],"%s",outfile);
-            strcpy(currInput->output, outfile);
+            //char* outfile;
+            //sscanf(inputs[i],"%s",outfile);
+            //strcpy(currInput->output, outfile);
+            strcpy(currInput->output, inputs[i]);
+          }
+          /* Background or foreground process */
+          else if (strcmp(inputs[i], "&\n") == 0) {
+            background = true;
           }
           /* Arguments */
           else {
-            //strcpy(currInput->args[inputarg] = inputs[i]);
+            //char* arg;
+            //sscanf(inputs[i], "%s", arg);
+            //currInput->args[inputarg] = arg;
             currInput->args[inputarg] = inputs[i];
             inputarg++;
           }
         }
+        /* Debugging */
         printf("Command: %s\n", currInput->command);
         printf("Output: %s\n", currInput->output);
         printf("Input: %s\n", currInput->input);
+        printf("Background process? %d\n", background);
         int i = 0;
         while (currInput->args[i] != NULL) {
           printf("Arguments: %s\n", currInput->args[i]); 
           i++;
+
+        
         }
       }
     }
