aff3f1508ca9ad61d786e45ccbffd503ce4a5b52
fusakoo <obatafusako@gmail.com>
2022-05-09 23:11:40 -0700

    Do as much modularization as possible

--- smallsh.c
+++ smallsh.c
@@ -16,77 +16,39 @@
 #define ARGSIZE 512
 #define MAXCHILDREN 30
 
-/*
- * TODO
- * Modularization
- */
-
+// Global variables for signal handler use
 volatile sig_atomic_t gRunForeground = 1;
-volatile sig_atomic_t gForegroundActive = 0;
 volatile sig_atomic_t gForegroundMode = 0;
 
-struct input{
+struct input {
   char* command;
   char* args[ARGSIZE];
   char* input;
   char* output;
 };
 
-// Handler for SIGINT
-void handle_SIGINT(int signo) {
-  if ((gForegroundActive == 1) && (gRunForeground == 1)) {
-    gForegroundActive = 0;
-    gRunForeground = 0;
-    char* message = "\nTerminated by signal 2\n";
-    write(STDOUT_FILENO, message, 24);
-  }
-}
+// Function declaration
+void handle_SIGINT(int signo);
+void handle_SIGTSTP(int signo);
+void check_bg_process(pid_t pids[], int status, int childcount);
+int *var_expansion(char *input);
+void remove_newline(char *input);
+void exit_shell(pid_t pids[]);
+void change_dir(char *argument);
 
-// Handler for SIGTSTP
-void handle_SIGTSTP(int signo) {
-  if (gForegroundMode == 0) {
-    char* entryMessage = "\nEntering foreground-only mode (& is now ignored)\n";
-    write(STDOUT_FILENO, entryMessage, 50);
-    gForegroundMode = 1;
-  }
-  else {
-    char* exitMessage = "\nExiting foreground-only mode\n";
-    write(STDOUT_FILENO, exitMessage, 30);
-    gForegroundMode = 0;  
-  }
-}
-
-// Function to process $$ expansion to pid
-int *var_expansion(char *input) {
-  pid_t pid = getpid();
-  char* pidstr;
-  /* Reference: Ed Discussion post #355 */
-  {
-    int n = snprintf(NULL, 0, "%d", pid);
-    pidstr = malloc((n + 1) * sizeof *pidstr);
-    sprintf(pidstr, "%d", pid);
-  }
-  int pidlen = strlen(pidstr);
-  while(1){
-    char *p = strstr(input, "$$");
-    if (p != NULL) {
-      memmove(p + pidlen, p + 2, strlen(p+2) + 2);
-      memcpy(p, pidstr, pidlen);
-    } else {
-      break;
-    } 
-  }
-  return 0; 
-}
-
-int main(){
+// Main method
+int main() {
   printf("Welcome to smallsh!\n");
   fflush(stdout);
 
+  // Used for status tracking
   int status;
+  
+  // pids stored in an array to tracki child pids (for background process tracking)
   pid_t pids[MAXCHILDREN] = {0};
   int childcount = 0;
-
+  
+  // Reference: Process Concept & States
   pid_t parentPid = getpid();
   char buffer[BUFFSIZE];
 
@@ -114,28 +76,12 @@ int main(){
     sigfillset(&SIGINT_action.sa_mask);
     SIGINT_action.sa_flags = 0;
 
-    // Initialize SIGINT with ignore_action handler to ignore SIGINT until process
+    // Initialize SIGINT with ignore_action handler to ignore ctrl + c until process
     ignore_action.sa_handler = SIG_IGN;
     sigaction(SIGINT, &ignore_action, NULL);
-
-    /* Check status of the background process */
-    for (int i = 0; i < MAXCHILDREN; i++) {
-      if (pids[i] != 0){
-        if (waitpid(pids[i], &status, WNOHANG) != 0) {
-          printf("Background pid %d complete: ", pids[i]);
-          fflush(stdout);
-          if (WIFEXITED(status)) {
-            printf("Exit status %d\n", WEXITSTATUS(status));
-            fflush(stdout);
-          } else {
-            printf("Terminated by signal %d\n", WTERMSIG(status));
-            fflush(stdout);
-          }
-          pids[i] = 0;
-          childcount--;
-        }
-      }
-    }
+    
+    // Run a check for background child process(es)
+    check_bg_process(pids, status, childcount);
     
     /*
     * 1. Command Prompt
@@ -159,9 +105,9 @@ int main(){
       int inputarg = 0;
       const char delim[2] = " ";
       char* token;
-      token = strtok(buffer, delim);
       bool ignoreCommand = false;
-
+      
+      token = strtok(buffer, delim);
       /* Handle arguments (and blank inputs) */
       while ( token != NULL ) {
         if (strncmp(token, "\n", 1) == 0) {
@@ -179,21 +125,15 @@ int main(){
 
       /* Check if the input is a comment (#) */
       if (ignoreCommand == false) {
-        /* Struct based on input to process command line */
+        // Background process tracker (turns true if & is detected)
         bool background = false;
         
+        /* Struct based on input to process command line */
         struct input* currInput = malloc(sizeof(struct input));
         for (int i = 0; i < inputsize; i++) {
-          /* ToDo
-          * Modularize the clean up 
-          * Handle "   " (space) inputs
-          */
-
-          /* Clean this code later */
+          // Cleans input (part of commandline) before parsing through checks 
           if (i == inputsize - 1) {
-            char temp[strlen(inputs[i])];
-            sscanf(inputs[i], "%s", temp);
-            strcpy(inputs[i],temp);
+            remove_newline(inputs[i]);
           }
           /* Command */
           if (i == 0) {
@@ -203,11 +143,8 @@ int main(){
           /* Input file */
           else if (strcmp(inputs[i], "<") == 0) {
             i++;
-            /* Clean this code later */
             if (i == inputsize - 1) {
-              char temp[strlen(inputs[i])];
-              sscanf(inputs[i], "%s", temp);
-              strcpy(inputs[i],temp);
+              remove_newline(inputs[i]);
             }
             currInput->input = calloc(strlen(inputs[i]) + 1, sizeof(char));
             strcpy(currInput->input, inputs[i]);
@@ -215,11 +152,8 @@ int main(){
           /* Output file */
           else if (strcmp(inputs[i], ">") == 0) {
             i++;
-            /* Clean this code later */
             if (i == inputsize - 1) {
-              char temp[strlen(inputs[i])];
-              sscanf(inputs[i], "%s", temp);
-              strcpy(inputs[i],temp);
+              remove_newline(inputs[i]);
             }
             currInput->output = calloc(strlen(inputs[i]) + 1, sizeof(char));
             strcpy(currInput->output, inputs[i]);
@@ -243,14 +177,8 @@ int main(){
 
         /* exit */
         if (strcmp(currInput->command,"exit") == 0) {
-          printf("Exiting smallsh!\n");
-          fflush(stdout);
-          // Kill any child process before exiting
-          for (int i = 0; i < MAXCHILDREN; i++) {
-            if (pids[i] != 0) {
-              kill(pids[i], SIGTERM);
-            }
-          }
+          exit_shell(pids);
+          // Free at least what will always be used
           free(currInput->command);
           free(currInput);
           exit(0);
@@ -258,22 +186,13 @@ int main(){
         }
         /* cd */
         else if (strcmp(currInput->command, "cd") == 0) {
-          if (currInput->args[0] == NULL) {
-            chdir(getenv("HOME"));
-            char cwd[100];
-            printf("Currently in: %s\n", getcwd(cwd,sizeof(cwd)));
-            fflush(stdout);
-          } else {
-            chdir(currInput->args[0]);
-            char cwd[100];
-            printf("Currently in: %s\n", getcwd(cwd,sizeof(cwd)));
-            fflush(stdout);
-          }
+          change_dir(currInput->args[0]);  
         }
         /* status */
         // Reference: Exploration: Process API - Monitoring Child Processes
         else if (strcmp(currInput->command, "status") == 0) {
           waitpid(parentPid, &status, WNOHANG);
+          // Check status and print accordingly (exit status vs termination signal)
           if (WIFEXITED(status)) {
             printf("Exit status %d\n", WEXITSTATUS(status));
             fflush(stdout);
@@ -288,7 +207,8 @@ int main(){
         else {
           sigaction(SIGINT, &SIGINT_action, NULL);
           
-          /* Command + arguments + NULL */
+          // Create arg vector for execvp()
+          // Command + arguments + NULL
           int component = 1 + inputarg + 1;
           char *newargv[component];
           newargv[0] = currInput->command;
@@ -300,7 +220,7 @@ int main(){
             }
           }
           newargv[component - 1] = NULL;
-          
+         
           // Prevent forking if max number of child processes has been reached
           if (childcount == 10) {
             err(errno=EAGAIN, "Max number of child process reached");
@@ -311,8 +231,10 @@ int main(){
 
           pid_t spawnPid = fork();
 
+          /* Fork a child */
           switch(spawnPid) {
           case -1:
+            /* Fork failed */
             perror("Fork() failed");
             fflush(stderr);
             exit(1);
@@ -323,8 +245,11 @@ int main(){
             /*
              * 6. Input and output redirection
              */
+            // Reference: Exploration - Processes and I/O
+                
             /* Input */
             if (currInput->input != NULL ) {
+              // Input is specified -> process + dup()
               int sourceFD = open(currInput->input, O_RDONLY);
               fcntl(sourceFD, F_SETFD, FD_CLOEXEC);
               if (sourceFD == -1){
@@ -340,7 +265,7 @@ int main(){
               }
             }
             else {
-              // No input dest specified
+              // No input specified
               // If background process -> direct to /dev/nul
               if (background == true) {
                 int sourceFD = open("/dev/null", O_RDONLY);
@@ -360,6 +285,7 @@ int main(){
             }
             /* Output */
             if (currInput->output != NULL) {
+              // Output dest is specified -> process + dup()
               int targetFD = open(currInput->output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
               fcntl(targetFD, F_SETFD, FD_CLOEXEC);
               if (targetFD == -1){
@@ -393,12 +319,15 @@ int main(){
                 }
               }
             }
+            /* Execute the command */
             execvp(newargv[0], newargv);
+            // Error if it fails to execute -> perror and exit with 2
             perror(newargv[0]);
             fflush(stderr);
             exit(2);
             break;
           default:
+            // Reassign signal handler to SIGINT_action so ctrl + c is accepted
             sigaction(SIGINT, &SIGINT_action, NULL);
             
             /* Background process */
@@ -406,7 +335,6 @@ int main(){
             if (background == 1) {
               printf("Running the command in the background (%d).\n", spawnPid);
               fflush(stdout);
-              //store_pid(pids, spawnPid, childcount);
               for (int i = 0; i < MAXCHILDREN; i++) {
                 if (pids[i] == 0) {
                   pids[i] = spawnPid;
@@ -425,7 +353,6 @@ int main(){
             else {
               /* Foreground process */
               gRunForeground = 1;
-              gForegroundActive = 1;
               while (gRunForeground) {
                 spawnPid = waitpid(spawnPid, &status, 0);
                 gRunForeground = 0;
@@ -437,8 +364,109 @@ int main(){
         }
       }
     }
-    /* If there's no input, loop again */
   }
-
   return 0;
+}
+
+// Handler for SIGINT
+void handle_SIGINT(int signo) {
+  // Checks to see if a foreground process is active & running
+  if (gRunForeground == 1) {
+    gRunForeground = 0;
+    char* message = "\nTerminated by signal 2\n";
+    write(STDOUT_FILENO, message, 24);
+  }
+}
+
+// Handler for SIGTSTP
+void handle_SIGTSTP(int signo) {
+  if (gForegroundMode == 0) {
+    char* entryMessage = "\nEntering foreground-only mode (& is now ignored)\n";
+    write(STDOUT_FILENO, entryMessage, 50);
+    gForegroundMode = 1;
+  }
+  else {
+    char* exitMessage = "\nExiting foreground-only mode\n";
+    write(STDOUT_FILENO, exitMessage, 30);
+    gForegroundMode = 0;  
+  }
+}
+
+// Function to check status of the background process(es)
+void check_bg_process(pid_t pids[], int status, int childcount) {
+  for (int i = 0; i < MAXCHILDREN; i++) {
+    if (pids[i] != 0){
+      if (waitpid(pids[i], &status, WNOHANG) != 0) {
+        printf("Background pid %d ended: ", pids[i]);
+        fflush(stdout);
+        if (WIFEXITED(status)) {
+          printf("Exit status %d\n", WEXITSTATUS(status));
+          fflush(stdout);
+        } else {
+          printf("Terminated by signal %d\n", WTERMSIG(status));
+          fflush(stdout);
+        }
+        pids[i] = 0;
+        childcount--;
+      }
+    }
+  }
+}
+
+// Function to process $$ expansion to pid
+int *var_expansion(char *input) {
+  pid_t pid = getpid();
+  char* pidstr;
+  // Reference: Ed Discussion post #355
+  {
+    int n = snprintf(NULL, 0, "%d", pid);
+    pidstr = malloc((n + 1) * sizeof *pidstr);
+    sprintf(pidstr, "%d", pid);
+  }
+  int pidlen = strlen(pidstr);
+  while(1){
+    char *p = strstr(input, "$$");
+    if (p != NULL) {
+      memmove(p + pidlen, p + 2, strlen(p+2) + 2);
+      memcpy(p, pidstr, pidlen);
+    } else {
+      break;
+    } 
+  }
+  return 0; 
+}
+
+// Function to remove newline at the end of commandline input
+void remove_newline(char *input) {
+  char temp[strlen(input)];
+  sscanf(input, "%s", temp);
+  strcpy(input,temp);
+}
+
+// Function that exits out of shell
+void exit_shell(pid_t pids[]) {
+  printf("Exiting smallsh!\n");
+  fflush(stdout);
+  // Kill any child process before exiting
+  for (int i = 0; i < MAXCHILDREN; i++) {
+    if (pids[i] != 0) {
+      kill(pids[i], SIGTERM);
+    }
+  }
+}
+
+// Function that mimics cd (changes directory to specified OR go to HOME directory)
+void change_dir(char *argument) {
+  if (argument == NULL) {
+    // Reference: Exploration - Environment
+    chdir(getenv("HOME"));
+    char cwd[100];
+    printf("Currently in: %s\n", getcwd(cwd,sizeof(cwd)));
+    fflush(stdout);
+  } else {
+    chdir(argument);
+    char cwd[100];
+    printf("Currently in: %s\n", getcwd(cwd,sizeof(cwd)));
+    fflush(stdout);
+  }
 }
